f0bd6d932c6e100e65f25716bba92d4d1e8cc44f
soumyaranjanmohanty007@gmail.com <soumyaranjanmohanty007@gmail.com>
2020-06-16 15:14:05 +0530

    Added java src files

---
+++ src/main/java/com/websystique/springmvc/dao/ConnectionDao.java
@@ -0,0 +1,51 @@
+package com.websystique.springmvc.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.websystique.springmvc.model.User;
+
+public class ConnectionDao {
+	Connection con = null;
+	public Connection RetriveConnection() {
+	try {
+		// Class.forName("com.mysql.jdbc.Driver");
+		Class.forName("org.postgresql.Driver");  
+		con=DriverManager.getConnection(  
+		// "jdbc:mysql://awsrds.cuaofirbrh2o.us-west-2.rds.amazonaws.com:3306/user","postgres","postgres123");
+		//jdbc:postgresql://localhost/test?user=fred&password=secret
+		"jdbc:postgresql://3.7.225.11:5432/user?user=postgres&password=postgres123");	
+		}catch(Exception e){ System.out.println(e);}
+	return con;  
+	}
+	
+	public List<User> getUserList(Connection conn){
+		if(conn == null) {
+			conn = RetriveConnection();
+		}
+		List<User> userList = new ArrayList<>();
+		try {
+		Statement stmt=conn.createStatement();  
+		ResultSet rs=stmt.executeQuery("select * from userinfo");  
+		while(rs.next())  {
+			User user = new User();
+			user.setId(rs.getInt(1));
+			user.setUsername(rs.getString(2));
+			user.setAddress(rs.getString(3));
+			user.setEmail(rs.getString(4));
+			userList.add(user);
+		}
+		System.out.println("list is: "+userList.size());
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return userList;
+		//con.close();  
+		
+	}
+
+}
